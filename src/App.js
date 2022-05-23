04b6e5cf0c26906601f7dd75408d91abf87c9624
George Pickett <gpickett00@gmail.com>
2022-05-23 12:46:20 +0300

    template project for new parcel project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'React'
+
+import "./styles.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+
+  useEffect(() => {
+    console.log('hey');
+  },[])
+
+  return (
+    <>
+      <div>sup</div>
+    </>
+  )
+}
+
+export { App }
+
